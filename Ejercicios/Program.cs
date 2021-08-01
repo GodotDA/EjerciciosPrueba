35e4c09ec12aab92ea4289983a807bc41376ea49
Godot <godotxr@gmail.com>
2021-08-01 17:01:51 +0200

    Ejercicio 2

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -2,9 +2,16 @@
 
 namespace Ejercicios
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
+        {
+            //Program.CalcularVolumenCilindro();
+            Program.CalcuarPrecioMinimoDeUnProducto();
+        }
+
+        // Ejercicio 1
+        public static void CalcularVolumenCilindro()
         {
             double diametro = ConseguirInformacion.ConseguirDiametro();
             double altura = ConseguirInformacion.ConseguirAltura();
@@ -13,5 +20,29 @@ namespace Ejercicios
 
             Console.WriteLine("El volumen es: " + volumen);
         }
+
+        // Ejercicio 2
+        public static void CalcuarPrecioMinimoDeUnProducto()
+        {
+            Console.WriteLine("Dame el precio del primer producto");
+            string producto1String = Console.ReadLine();
+            double producto1 = Double.Parse(producto1String);
+            Console.WriteLine("Dame el precio del segundo producto");
+            string producto2String = Console.ReadLine();
+            double producto2 = Double.Parse(producto2String);
+            Console.WriteLine("Dame el precio del tercer producto");
+            string producto3String = Console.ReadLine();
+            double producto3 = Double.Parse(producto3String);
+            if (producto1 <= producto2 && producto1 <= producto3)
+            {
+                Console.WriteLine("El producto 1 es más barto");
+            }
+            else if (producto2 <= producto1 && producto2 <= producto3)
+            {
+                Console.WriteLine("El prodcuto 2 es más barato");
+            }
+            else Console.WriteLine("El producto 3 es más barato");
+        }
+
     }
 }
